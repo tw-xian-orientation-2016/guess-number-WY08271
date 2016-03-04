a4e932d1e8667ea49d4edd340f4d40a75465f54e
WY08271 <wy08271@outlook.com>
2016-03-04 11:20:20 +0800

    modify CompareNumberTest.java

--- test/CompareNumberTest.java
+++ test/CompareNumberTest.java
@@ -2,6 +2,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class CompareNumberTest {
@@ -23,15 +24,22 @@ public class CompareNumberTest {
 
     @Test
     public void output_4A0B_when_answer_all_match() {
-        String result = compareNumber.compare("1234", "5678");
+        String result = compareNumber.compare("1234", "1234");
 
-        assertThat(result, is("0A0B"));
+        assertThat(result, is("4A0B"));
     }
 
     @Test
     public void output_0A4B_when_answer_location_match() {
-        String result = compareNumber.compare("1234", "5678");
+        String result = compareNumber.compare("1234", "4321");
 
-        assertThat(result, is("0A0B"));
+        assertThat(result, is("0A4B"));
+    }
+
+    @Test
+    public void output_error_when_answer_and_number_match() {
+        String result = compareNumber.compare("1234", "2354");
+
+        assertThat(result, not("0A0B"));
     }
 }
